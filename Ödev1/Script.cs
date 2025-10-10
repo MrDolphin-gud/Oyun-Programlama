556b8b30deb9fabce2916ffa0c260aeb7ce53b46
Yunus Ayaz <150587761+MrDolphin-gud@users.noreply.github.com>
2025-10-10 20:47:02 +0300

    Add files via upload

---
+++ Ödev1/Script.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int hız = 5;
+    Vector3 pozisyon = new Vector3(2, 0, 0);
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yatay = Input.GetAxis("Horizontal");
+        float dikey = Input.GetAxis("Vertical");
+        transform.position += new Vector3(yatay * Time.deltaTime * hız, dikey * Time.deltaTime * hız, 0);
+    }
+}
